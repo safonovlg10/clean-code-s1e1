ec8478ae0ca07ed0d12bf8879668044457c1143f
safonovlg10 <safonovlg10@gmail.com>
2024-12-17 20:52:38 +0300

    refactor(basic-3.4) replacing selector tag with classes

--- app.js
+++ app.js
@@ -32,19 +32,21 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = "task-item";
     label.innerText=taskString;
-    label.className='task';
+    label.className='task task-lable';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit task-button";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete task-button";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "image-delete";
     deleteButton.appendChild(deleteButtonImg);
 
 
